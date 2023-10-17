60f42b9f61eddee731edbc09473afe285389e753
Lennon Machado <lennon2049@gmail.com>
2023-10-17 19:45:04 +0000

    Qinta Parte

--- app/src/main/java/application/model/Aluno.java
+++ app/src/main/java/application/model/Aluno.java
@@ -1,31 +1,35 @@
 package application.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="alunos")
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private int idade;
 
     public int getId() {
-        return this.id;
+        return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNome() {
-        return this.nome;
+        return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public int getIdade() {
-        return this.idade;
+        return idade;
     }
-
     public void setIdade(int idade) {
         this.idade = idade;
     }
